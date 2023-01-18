b520a3986fa9d3a29dd15009eb42db45fab99ce5
James Thigpen <james.r.thigpen@gmail.com>
2023-01-18 11:04:20 -0800

    Update cloramode.js

--- cloramode.js
+++ cloramode.js
@@ -1,1 +1,14 @@
 console.log('Loading Clora Mode Utils');
+
+_cloramode = {};
+_cloramode.setTitle = function(queryName, filterName, domTarget, modeDatasets) {
+  setTimeout(function() {
+    var d = modeDatasets.filter(function(d) {
+      return d.queryName == queryName;
+    });
+    var numConsultants = d[0].count;
+    var consultantName =d[0].content[0][filterName];
+    
+    $(domTarget).text(consultantName);
+  }, 500);
+};
